f242fc42943335239fe5739c981f3f9863cddcdc
Heather Nuffer <zzzuzz@gmail.com>
2020-01-24 16:22:19 -0700

    Added server

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const UsersRouter = require('../users/users-router.js');
+
+const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('tiny'));
+server.use(cors());
+
+server.use('/api/users', UsersRouter);
+
+module.exports = server;
---
+++ users/users-router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const Users = require('./users-model.js');
+const router = express.Router();
+
+router.get('/', (req, res) => res.status(200).send('Welcome to the Users Router'));
+
+module.exports = router;
